fa75dece52d210ff5a0b0368c2b41a17b6be83d4
kaush26 <kaushalprasad2611@gmail.com>
2024-02-18 00:55:19 +0530

    first commit

---
+++ .prettierrc.config.js
@@ -0,0 +1,9 @@
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 126,
+};
+
+module.exports = config;
---
+++ components/Button.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Button() {
+  return <button className="border border-red-500 p-[13px]">Button</button>;
+}
